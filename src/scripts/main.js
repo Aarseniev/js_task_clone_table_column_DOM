481ee843bcdb4bec720f516d3a34d932700bd841
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-03 01:49:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const mainTable = document.querySelector('table');
+
+const allRows = mainTable.querySelectorAll('tr');
+
+allRows.forEach((row) => {
+  const cells = row.querySelectorAll('td, th');
+  const secondCell = cells[1];
+  const lastCell = cells[cells.length - 1];
+
+  const clonedCell = secondCell.cloneNode(true);
+
+  row.insertBefore(clonedCell, lastCell);
+});

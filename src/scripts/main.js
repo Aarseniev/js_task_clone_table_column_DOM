a697566d80bdc062902d90f454494a48b5b13678
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-04 00:22:30 +0300

    fixed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,47 +7,53 @@ function cloneRow() {
   const tBody = mainTable.querySelector('tbody');
   const tFoot = mainTable.querySelector('tFoot');
 
-  tHead.querySelectorAll('tr').forEach((row) => {
-    const cells = row.querySelectorAll('th');
-    const secondCell = cells[1];
-    const lastCell = cells[cells.length - 1];
+  if (tHead) {
+    tHead.querySelectorAll('tr').forEach((row) => {
+      const cells = row.querySelectorAll('th, td');
+      const secondCell = cells[1];
+      const lastCell = cells[cells.length - 1];
 
-    if (row.cells.length < 2) {
-      return;
-    }
+      if (row.cells.length < 2) {
+        return;
+      }
 
-    const clonedCell = secondCell.cloneNode(true);
+      const clonedCell = secondCell.cloneNode(true);
 
-    row.insertBefore(clonedCell, lastCell);
-  });
+      row.insertBefore(clonedCell, lastCell);
+    });
+  }
 
-  tBody.querySelectorAll('tr').forEach((row) => {
-    const cells = row.querySelectorAll('td');
-    const secondCell = cells[1];
-    const lastCell = cells[cells.length - 1];
+  if (tBody) {
+    tBody.querySelectorAll('tr').forEach((row) => {
+      const cells = row.querySelectorAll('th, td');
+      const secondCell = cells[1];
+      const lastCell = cells[cells.length - 1];
 
-    if (row.cells.length < 2) {
-      return;
-    }
+      if (row.cells.length < 2) {
+        return;
+      }
 
-    const clonedCell = secondCell.cloneNode(true);
+      const clonedCell = secondCell.cloneNode(true);
 
-    row.insertBefore(clonedCell, lastCell);
-  });
+      row.insertBefore(clonedCell, lastCell);
+    });
+  }
 
-  tFoot.querySelectorAll('tr').forEach((row) => {
-    const cells = row.querySelectorAll('th');
-    const secondCell = cells[1];
-    const lastCell = cells[cells.length - 1];
+  if (tFoot) {
+    tFoot.querySelectorAll('tr').forEach((row) => {
+      const cells = row.querySelectorAll('th, td');
+      const secondCell = cells[1];
+      const lastCell = cells[cells.length - 1];
 
-    if (row.cells.length < 2) {
-      return;
-    }
+      if (row.cells.length < 2) {
+        return;
+      }
 
-    const clonedCell = secondCell.cloneNode(true);
+      const clonedCell = secondCell.cloneNode(true);
 
-    row.insertBefore(clonedCell, lastCell);
-  });
+      row.insertBefore(clonedCell, lastCell);
+    });
+  }
 }
 
 cloneRow();
